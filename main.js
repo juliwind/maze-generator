abad4a87f99b96b4ee6847635e39c4dce323f41b
juliwind <julianwindheuser@gmail.com>
2021-08-15 16:02:34 +0200

    code finished, try to start

--- main.js
+++ main.js
@@ -1,97 +1,100 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
-let final_resolution = 1;
-final_resolution = prompt("Enter your final resolution: ", );
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const final_resolution = prompt("Enter your final resolution: ", );
 
-init();
-function init() {
-    let field = new Array();
-    for (i = 0; i < canvas.height / final_resolution; i++) {
-        field.push(new Array());
-        for (j = 0; j < canvas.width / final_resolution; j++) {
-            field[i].push("0");
-        }  
-    }
-    ctx.lineWidth = 2;
-    start(field);
+function start() {
+    let field = Array(Math.round(canvas.height / final_resolution)).fill().map(() => Array(Math.round(canvas.width / final_resolution)).fill(0));
+    let horizontal = isHorizontal(field.length, field[0].length);
+
+    boarder(field)
+    recursiveDivision(field, horizontal, 1, field[0].length, 1, field.length);
+    draw(field);
+    console.log(field)
 }
 
-function start(field) {
-    recursiveDivision(field, canvas.width, canvas.height, 0, canvas.width, 0, canvas.height);
-}
+function recursiveDivision(field, horizontal, min_x, max_x, min_y, max_y) {
+    console.log(field)
+    if (horizontal) {
+        if (max_x - min_x <= final_resolution) {
+            return;
+        }
+        let random_y = RandomNumberInRange(max_x, max_y)
+        horizontalWall(min_x, max_x, random_y, field);
 
-function recursiveDivision(field, width, height, width_min, width_max, height_min, height_max) {
-    console.log(width, height)
-    if(width <= final_resolution || height <= final_resolution) {
-        return field;
-    }
-    let direction = selectDirection(width, height);
-    let wallX = RandomNumberInRange(width_min, width_max);
-    let wallY = RandomNumberInRange(height_min, height_max);
-    drawWall(direction, width_min, width_max, height_min, height_max, wallX, wallY);
-
-    if(direction == "horizontal") {
-        recursiveDivision(field, width, height_min + wallY, width_min, width_max, height_min, wallY);
-        recursiveDivision(field, width, height_max - wallY, width_min, width_max, wallY, height_max);
-    }
-    else if(direction == "vertical") {
-        recursiveDivision(field, width_min + wallX, height, width_min, wallX, height_min, height_max);
-        recursiveDivision(field, width_max - wallX, height, wallX, width_max, height_min, height_max);
-    }
-
-    
-}
-//!
-//!
-function drawWall(direction, w_min, w_max, h_min, h_max, wallX, wallY) { //w = width, h = height
-    if(direction == "horizontal") {
-        let door_coord_min = setDoorCoordinates(h_min, h_max);
-        let door_coord_max = door_coord_min + final_resolution;
-        ctx.beginPath();
-        ctx.moveTo(w_min, wallY);
-        ctx.lineTo(door_coord_min, wallY);
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(w_max, wallY);
-        ctx.lineTo(door_coord_max, wallY);
-        ctx.stroke();
+        recursiveDivision(field, !horizontal, min_x, max_x, min_y, random_y);
+        recursiveDivision(field, !horizontal, min_x, max_x, random_y, max_y);
     }
     else {
-        let door_coord_min = setDoorCoordinates(w_min, w_max);
-        let door_coord_max = door_coord_min + final_resolution;
-        ctx.beginPath();
-        ctx.moveTo(wallX, w_min);
-        ctx.lineTo(wallX, door_coord_min);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(wallX, w_max);
-        ctx.lineTo(wallX, door_coord_max);
-        ctx.stroke();
-    }
+        if (max_y - min_y <= final_resolution) {
+            return;
+        }
+        let random_x = RandomNumberInRange(min_x, max_x)
+        verticalWall(min_y, max_y, random_x, field);
 
+        recursiveDivision(field, !horizontal, min_x, random_x, min_y, max_y);
+        recursiveDivision(field, !horizontal, random_x, max_x, min_y, max_y);
+    }
 }
 
-function selectDirection(width, height) {
+function horizontalWall(min_x, max_x, y, field) {
+    let door = RandomNumberInRange(min_x, max_x);
+    for (let x = min_x; x <= max_x; i++) {
+        if (x != door) {
+            field[x][y] = "w"
+        }
+    }
+}
+
+function verticalWall(min_y, max_y, x, field) {
+    let door = RandomNumberInRange(min_y, max_y);
+    for (let y = min_y; y <= max_y; y++) {
+        if (y != door) {
+            field[x][y] = "w"
+        }
+    }
+}
+
+function isHorizontal(width, height) {
     if(width < height) {
-        return "horizontal";
+        return true;
     }
     else {
-        return "vertical";
+        return false;
     }
 }
-function setDoorCoordinates(value_min, value_max) {
-    value_max = value_max - final_resolution;
-    let rNumber = RandomNumberInRange(value_min, value_max);
-    return rNumber;
+
+function boarder(field) {
+    for (i = 0; i < field.length; i++) {
+        if (i == 0 || i == field.length - 1) {
+            for (j = 0; j < field[0].length; j++) {
+                field[i][j] = "w";
+            }
+        }
+        else {
+            field[i][field[0].length - 1] = "w";
+            field[i][0] = "w";
+        }
+    }
 }
+
+function draw(field) {
+    for (i = 0; i < field.length; i++) {
+        for (j = 0; j < field[0].length; j++) {
+            let x = final_resolution * j;
+            let y = final_resolution * i;
+            switch (field[i][j]) {
+                case "w":
+                    ctx.beginPath();
+                    ctx.fillRect(x, y, final_resolution, final_resolution);
+                    break;
+            }
+        }
+    }
+}
+
 function RandomNumberInRange(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     let rNumber = Math.floor(Math.random() * (max - min)) + min;
     return rNumber;
 }
-
-  /*
-
-  */
