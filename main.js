c255cac41c2267974f3a915a7b4c3f42c20d1ea0
juliwind <julianwindheuser@gmail.com>
2021-08-16 18:16:22 +0200

    finally finished

--- main.js
+++ main.js
@@ -3,60 +3,81 @@ const ctx = canvas.getContext("2d");
 const final_resolution = prompt("Enter your final resolution: ", );
 
 function start() {
-    let field = Array(Math.round(canvas.height / final_resolution)).fill().map(() => Array(Math.round(canvas.width / final_resolution)).fill(0));
+    let field = [];
+    let y = canvas.height / final_resolution;
+    if (y % 2 == 1) {
+        y++;
+    }
+    let x = canvas.width / final_resolution;
+    if (x % 2 == 1) {
+        x++
+    }
+    for (i = 0; i < y; i++) {
+        field.push(new Array());
+        for (j = 0; j < x; j++) {
+            field[i].push("0");
+        }  
+    }
     let horizontal = isHorizontal(field.length, field[0].length);
 
     boarder(field)
-    recursiveDivision(field, horizontal, 1, field[0].length, 1, field.length);
+    recursiveDivision(field, horizontal, 1, field[0].length - 2, 1, field.length - 2);
     draw(field);
-    console.log(field)
 }
 
 function recursiveDivision(field, horizontal, min_x, max_x, min_y, max_y) {
-    //console.log(max_x, max_y)
-    console.log("HERE I AM")
     if (horizontal) {
-        console.log("HOR")
         if (max_x - min_x < 2) {
-            console.log("FINISHED")
             return;
         }
-        let random_y = RandomNumberInRange(max_x, max_y)
-        //console.log(random_y)
+        let random_y = RandomNumberInRange(min_y, max_y - 1)
+        if (random_y % 2 == 1) {
+            random_y++;
+        }
         horizontalWall(min_x, max_x, random_y, field);
-
-        recursiveDivision(field, !horizontal, min_x, max_x, min_y, random_y);
-        recursiveDivision(field, !horizontal, min_x, max_x, random_y, max_y);
+        recursiveDivision(field, !horizontal, min_x, max_x, min_y, random_y- 1);
+        recursiveDivision(field, !horizontal, min_x, max_x, random_y + 1, max_y);
     }
     else {
-        console.log("VER")
         if (max_y - min_y < 2) {
-            console.log("FINISHED")
             return;
         }
-        let random_x = RandomNumberInRange(min_x, max_x)
+        let random_x = RandomNumberInRange(min_x, max_x - 1)
+        if (random_x % 2 == 1) {
+            random_x++;
+        }
         verticalWall(min_y, max_y, random_x, field);
-
-        recursiveDivision(field, !horizontal, min_x, random_x, min_y, max_y);
-        recursiveDivision(field, !horizontal, random_x, max_x, min_y, max_y);
+        recursiveDivision(field, !horizontal, min_x, random_x - 1, min_y, max_y);
+        recursiveDivision(field, !horizontal, random_x + 1, max_x, min_y, max_y);
     }
 }
 
 function horizontalWall(min_x, max_x, y, field) {
-    let door = RandomNumberInRange(min_x, max_x);
-    console.log(min_x, max_x)
-    for (let x = min_x; x <= max_x; i++) {
+    let door = RandomNumberInRange(min_x, max_x - 1);
+    if (door % 2 == 0) {
+        door++;
+    }
+    for (let x = min_x; x <= max_x; x++) {
         if (x != door) {
-            field[x][y] = "w"
+            field[y][x] = "w";
+        }
+        else {
+            field[y][x] = "0";
         }
     }
 }
 
 function verticalWall(min_y, max_y, x, field) {
-    let door = RandomNumberInRange(min_y, max_y);
+    let door = RandomNumberInRange(min_y, max_y - 1);
+    if (door % 2 == 0) {
+        door++;
+    }
     for (let y = min_y; y <= max_y; y++) {
         if (y != door) {
-            field[x][y] = "w"
+            field[y][x] = "w";
+        }
+        else {
+            field[y][x] = "0";
         }
     }
 }
@@ -105,3 +126,4 @@ function RandomNumberInRange(min, max) {
     let rNumber = Math.floor(Math.random() * (max - min)) + min;
     return rNumber;
 }
+
